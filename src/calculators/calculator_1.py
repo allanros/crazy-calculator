ff63d7c764703e45bfee4e099b30eabe92b0ff36
Allan <allan.rosendo@hotmail.com>
2024-04-05 11:00:56 -0300

    adding calculator_1 logic & updating calculators class

---
+++ src/calculators/calculator_1.py
@@ -0,0 +1,39 @@
+from flask import Request as FlaskRequest
+from typing import Dict
+
+class Calculator1:
+    def calculate(self, request: FlaskRequest) -> Dict:
+        body = request.get_json()
+        input_data = self.__validate_body(body)
+        splited_number = input_data/3
+
+        first_process_result = self.__first_process(splited_number)
+        second_process_result = self.__second_process(splited_number)
+        calc_result = first_process_result + second_process_result + splited_number
+
+        return self.__format_response(calc_result)
+
+    def __validate_body(self, body: Dict) -> float:
+        if 'number' not in body:
+            raise Exception('Body bad formatted')
+        
+        input_data = body['number']
+        return input_data
+    
+    def __first_process(self, number: float) -> float:
+        number = (number/4) + 7
+        second_number = (number ** 2) * 0.257
+        return second_number
+        
+    def __second_process(self, second_number: float) -> float:
+        first_part = (second_number ** 2.121)
+        second_part = (first_part / 5) + 1
+        return second_part
+    
+    def __format_response(self, calc_result: float) -> Dict:
+        return {
+            'data': {
+                'Calculator': '1',
+                'result': round(calc_result, 2)
+            }
+        }
--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, jsonify, request
+from src.calculators.calculator_1 import Calculator1
 
 calc_route_bp = Blueprint('calc_routes', __name__)
 
 @calc_route_bp.route('/calculator/1', methods=['POST'])
 def calculador_1():
-    print(request.get_json())
-    return jsonify({'success': True})
+    calc = Calculator1()
+    response = calc.calculate(request)
+
+    return jsonify(response), 200
