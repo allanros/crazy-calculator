b4d6acaeb2e8472d04e240790ecc9e0d09993158
Allan <allan.rosendo@hotmail.com>
2024-04-09 16:35:36 -0300

    adding calculator2 logics & adding tests for calculator2

---
+++ src/calculators/calculator_2.py
@@ -0,0 +1,32 @@
+from flask import Request as FlaskRequest
+from typing import Dict, List
+from src.drivers.numpy_handler import NumpyHandler
+
+class Calculator2:
+    def calculate(self, request: FlaskRequest) -> Dict:
+        body = request.json
+        input_data = self.__validate_body(body)
+        calculated_number = self.__process_data(input_data)
+        return self.__format_response(calculated_number)
+
+    def __validate_body(self, body: Dict) -> List[float]:
+        if 'numbers' not in body:
+            raise Exception('Body bad formatted')
+        
+        input_data = body['numbers']
+        return input_data
+    
+    def __process_data(self, numbers: List[float]) -> float:
+        numpy_handler = NumpyHandler()
+        first_process_result = [(num * 11) ** .95 for num in numbers]
+        result = numpy_handler.standart_derivation(first_process_result)
+
+        return 1/result
+    
+    def __format_response(self, result: float) -> Dict:
+        return {
+            'data': {
+                'Calculator': '2',
+                'result': round(result, 2)
+            }
+        }
